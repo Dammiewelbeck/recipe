f8a38ac6f246f30d002d19083e3830e2412740f4
Dammiewelbeck <dammiewelbeck@gmail.com>
2024-04-04 20:47:30 +0100

    first commit

---
+++ recipes.php
@@ -0,0 +1,261 @@
+
+<?php include('header.php'); ?>
+
+  <div class="container">
+    <!-- Tstbite Components, My 4, My Md 5 -->
+    <section class="tstbite-components my-4 my-md-5">
+      <h5 class="py-3 mb-0">All Recipes</h5><hr/>
+      <!-- <div class="input-group search-box">
+        <input type="text" name="Search" placeholder="Search" class="form-control bg-white" id="Search2">
+        <span class="mt-2 mr-1">(98 Recipes)</span>
+        <button type="button"><img src="assets/images/icons/close.svg" alt="img"></button>
+      </div> -->
+      <div class="row">
+        <div class="col-lg-3 col-md-4 col-6">
+          <figure class="my-3 my-md-4 tstbite-card">
+            <a href="single-recipe.php" class="tstbite-animation stretched-link rounded-6">
+              <img src="assets/images/menus/menu117.jpg" class="w-100" alt="Menu">
+            </a>
+            <figcaption class="mt-2">
+              <a href="single-recipe.php" class="text-black d-block mt-1 font-weight-semibold big">Decadent Raspberry and Cream Cake</a>
+            </figcaption>
+          </figure>
+        </div>
+        <div class="col-lg-3 col-md-4 col-6">
+          <figure class="my-3 my-md-4 tstbite-card">
+            <a href="single-recipe.php" class="tstbite-animation stretched-link rounded-6">
+              <img src="assets/images/menus/menu118.jpg" class="w-100" alt="Menu">
+            </a>
+            <figcaption class="mt-2">
+              <a href="single-recipe.php" class="text-black d-block mt-1 font-weight-semibold big">Tripple Chocolate Mousse Cake</a>
+            </figcaption>
+          </figure>
+        </div>
+        <div class="col-lg-3 col-md-4 col-6">
+          <figure class="my-3 my-md-4 tstbite-card">
+            <a href="single-recipe.php" class="tstbite-animation stretched-link rounded-6">
+              <img src="assets/images/menus/menu119.jpg" class="w-100" alt="Menu">
+            </a>
+            <figcaption class="mt-2">
+              <a href="single-recipe.php" class="text-black d-block mt-1 font-weight-semibold big">Cranberry Curd Layered Sponge Cake</a>
+            </figcaption>
+          </figure>
+        </div>
+        <div class="col-lg-3 col-md-4 col-6">
+          <figure class="my-3 my-md-4 tstbite-card">
+            <a href="single-recipe.php" class="tstbite-animation stretched-link rounded-6">
+              <img src="assets/images/menus/menu120.jpg" class="w-100" alt="Menu">
+            </a>
+            <figcaption class="mt-2">
+              <a href="single-recipe.php" class="text-black d-block mt-1 font-weight-semibold big">Orange and Lemon Curd Tartlets</a>
+            </figcaption>
+          </figure>
+        </div>
+        <div class="col-lg-3 col-md-4 col-6">
+          <figure class="my-3 my-md-4 tstbite-card">
+            <a href="single-recipe.php" class="tstbite-animation stretched-link rounded-6">
+              <img src="assets/images/menus/menu121.jpg" class="w-100" alt="Menu">
+            </a>
+            <figcaption class="mt-2">
+              <a href="single-recipe.php" class="text-black d-block mt-1 font-weight-semibold big">Creamt Chocolate Nutella Fudge Cake</a>
+            </figcaption>
+          </figure>
+        </div>
+        <div class="col-lg-3 col-md-4 col-6">
+          <figure class="my-3 my-md-4 tstbite-card">
+            <a href="single-recipe.php" class="tstbite-animation stretched-link rounded-6">
+              <img src="assets/images/menus/menu122.jpg" class="w-100" alt="Menu">
+            </a>
+            <figcaption class="mt-2">
+              <a href="single-recipe.php" class="text-black d-block mt-1 font-weight-semibold big">Homemade Mixed Berries Wedding Cake</a>
+            </figcaption>
+          </figure>
+        </div>
+        <div class="col-lg-3 col-md-4 col-6">
+          <figure class="my-3 my-md-4 tstbite-card">
+            <a href="single-recipe.php" class="tstbite-animation stretched-link rounded-6">
+              <img src="assets/images/menus/menu123.jpg" class="w-100" alt="Menu">
+            </a>
+            <figcaption class="mt-2">
+              <a href="single-recipe.php" class="text-black d-block mt-1 font-weight-semibold big">Black Forest Birthday Cake</a>
+            </figcaption>
+          </figure>
+        </div>
+        <div class="col-lg-3 col-md-4 col-6">
+          <figure class="my-3 my-md-4 tstbite-card">
+            <a href="single-recipe.php" class="tstbite-animation stretched-link rounded-6">
+              <img src="assets/images/menus/menu124.jpg" class="w-100" alt="Menu">
+            </a>
+            <figcaption class="mt-2">
+              <a href="single-recipe.php" class="text-black d-block mt-1 font-weight-semibold big">Double Thick Layered Sponge Cake</a>
+            </figcaption>
+          </figure>
+        </div>
+        <div class="col-lg-3 col-md-4 col-6">
+          <figure class="my-3 my-md-4 tstbite-card">
+            <a href="single-recipe.php" class="tstbite-animation stretched-link rounded-6">
+              <img src="assets/images/menus/menu125.jpg" class="w-100" alt="Menu">
+            </a>
+            <figcaption class="mt-2">
+              <a href="single-recipe.php" class="text-black d-block mt-1 font-weight-semibold big">Lemon Cake with Chocolate Ganache</a>
+            </figcaption>
+          </figure>
+        </div>
+        <div class="col-lg-3 col-md-4 col-6">
+          <figure class="my-3 my-md-4 tstbite-card">
+            <a href="single-recipe.php" class="tstbite-animation stretched-link rounded-6">
+              <img src="assets/images/menus/menu126.jpg" class="w-100" alt="Menu">
+            </a>
+            <figcaption class="mt-2">
+              <a href="single-recipe.php" class="text-black d-block mt-1 font-weight-semibold big">Cranberry Macaroon Ice Cream Cake</a>
+            </figcaption>
+          </figure>
+        </div>
+        <div class="col-lg-3 col-md-4 col-6">
+          <figure class="my-3 my-md-4 tstbite-card">
+            <a href="single-recipe.php" class="tstbite-animation stretched-link rounded-6">
+              <img src="assets/images/menus/menu127.jpg" class="w-100" alt="Menu">
+            </a>
+            <figcaption class="mt-2">
+              <a href="single-recipe.php" class="text-black d-block mt-1 font-weight-semibold big">No Bake Cheesecake</a>
+            </figcaption>
+          </figure>
+        </div>
+        <div class="col-lg-3 col-md-4 col-6">
+          <figure class="my-3 my-md-4 tstbite-card">
+            <a href="single-recipe.php" class="tstbite-animation stretched-link rounded-6">
+              <img src="assets/images/menus/menu128.jpg" class="w-100" alt="Menu">
+            </a>
+            <figcaption class="mt-2">
+              <a href="single-recipe.php" class="text-black d-block mt-1 font-weight-semibold big">Almond Cinnamon Sponge Cake</a>
+            </figcaption>
+          </figure>
+        </div>
+        <div class="col-lg-3 col-md-4 col-6">
+          <figure class="my-3 my-md-4 tstbite-card">
+            <a href="single-recipe.php" class="tstbite-animation stretched-link rounded-6">
+              <img src="assets/images/menus/menu129.jpg" class="w-100" alt="Menu">
+            </a>
+            <figcaption class="mt-2">
+              <a href="single-recipe.php" class="text-black d-block mt-1 font-weight-semibold big">Mixed Candy Cake</a>
+            </figcaption>
+          </figure>
+        </div>
+        <div class="col-lg-3 col-md-4 col-6">
+          <figure class="my-3 my-md-4 tstbite-card">
+            <a href="single-recipe.php" class="tstbite-animation stretched-link rounded-6">
+              <img src="assets/images/menus/menu130.jpg" class="w-100" alt="Menu">
+            </a>
+            <figcaption class="mt-2">
+              <a href="single-recipe.php" class="text-black d-block mt-1 font-weight-semibold big">Cherry Ice Cream Cake</a>
+            </figcaption>
+          </figure>
+        </div>
+        <div class="col-lg-3 col-md-4 col-6">
+          <figure class="my-3 my-md-4 tstbite-card">
+            <a href="single-recipe.php" class="tstbite-animation stretched-link rounded-6">
+              <img src="assets/images/menus/menu131.jpg" class="w-100" alt="Menu">
+            </a>
+            <figcaption class="mt-2">
+              <a href="single-recipe.php" class="text-black d-block mt-1 font-weight-semibold big">Four Layer Coffee Cake</a>
+            </figcaption>
+          </figure>
+        </div>
+        <div class="col-lg-3 col-md-4 col-6">
+          <figure class="my-3 my-md-4 tstbite-card">
+            <a href="single-recipe.php" class="tstbite-animation stretched-link rounded-6">
+              <img src="assets/images/menus/menu132.jpg" class="w-100" alt="Menu">
+            </a>
+            <figcaption class="mt-2">
+              <a href="single-recipe.php" class="text-black d-block mt-1 font-weight-semibold big">Oreo Brownie Ice Cream Cake</a>
+            </figcaption>
+          </figure>
+        </div>
+        <div class="col-lg-3 col-md-4 col-6">
+          <figure class="my-3 my-md-4 tstbite-card">
+            <a href="single-recipe.php" class="tstbite-animation stretched-link rounded-6">
+              <img src="assets/images/menus/menu133.jpg" class="w-100" alt="Menu">
+            </a>
+            <figcaption class="mt-2">
+              <a href="single-recipe.php" class="text-black d-block mt-1 font-weight-semibold big">Caramel Glaze Cake</a>
+            </figcaption>
+          </figure>
+        </div>
+        <div class="col-lg-3 col-md-4 col-6">
+          <figure class="my-3 my-md-4 tstbite-card">
+            <a href="single-recipe.php" class="tstbite-animation stretched-link rounded-6">
+              <img src="assets/images/menus/menu134.jpg" class="w-100" alt="Menu">
+            </a>
+            <figcaption class="mt-2">
+              <a href="single-recipe.php" class="text-black d-block mt-1 font-weight-semibold big">No Bake Cinnamon Cheesecake</a>
+            </figcaption>
+          </figure>
+        </div>
+        <div class="col-lg-3 col-md-4 col-6">
+          <figure class="my-3 my-md-4 tstbite-card">
+            <a href="single-recipe.php" class="tstbite-animation stretched-link rounded-6">
+              <img src="assets/images/menus/menu135.jpg" class="w-100" alt="Menu">
+            </a>
+            <figcaption class="mt-2">
+              <a href="single-recipe.php" class="text-black d-block mt-1 font-weight-semibold big">Apple Cinnamon Bundt Cake</a>
+            </figcaption>
+          </figure>
+        </div>
+        <div class="col-lg-3 col-md-4 col-6">
+          <figure class="my-3 my-md-4 tstbite-card">
+            <a href="single-recipe.php" class="tstbite-animation stretched-link rounded-6">
+              <img src="assets/images/menus/menu136.jpg" class="w-100" alt="Menu">
+            </a>
+            <figcaption class="mt-2">
+              <a href="single-recipe.php" class="text-black d-block mt-1 font-weight-semibold big">Rainbow Explosion Birthday Cake</a>
+            </figcaption>
+          </figure>
+        </div>
+        <div class="col-lg-3 col-md-4 col-6">
+          <figure class="my-3 my-md-4 tstbite-card">
+            <a href="single-recipe.php" class="tstbite-animation stretched-link rounded-6">
+              <img src="assets/images/menus/menu137.jpg" class="w-100" alt="Menu">
+            </a>
+            <figcaption class="mt-2">
+              <a href="single-recipe.php" class="text-black d-block mt-1 font-weight-semibold big">Chocolate Peanut Butter Mini Cupcakes</a>
+            </figcaption>
+          </figure>
+        </div>
+        <div class="col-lg-3 col-md-4 col-6">
+          <figure class="my-3 my-md-4 tstbite-card">
+            <a href="single-recipe.php" class="tstbite-animation stretched-link rounded-6">
+              <img src="assets/images/menus/menu138.jpg" class="w-100" alt="Menu">
+            </a>
+            <figcaption class="mt-2">
+              <a href="single-recipe.php" class="text-black d-block mt-1 font-weight-semibold big">M&M’s Chocolate Cake</a>
+            </figcaption>
+          </figure>
+        </div>
+        <div class="col-lg-3 col-md-4 col-6">
+          <figure class="my-3 my-md-4 tstbite-card">
+            <a href="single-recipe.php" class="tstbite-animation stretched-link rounded-6">
+              <img src="assets/images/menus/menu139.jpg" class="w-100" alt="Menu">
+            </a>
+            <figcaption class="mt-2">
+              <a href="single-recipe.php" class="text-black d-block mt-1 font-weight-semibold big">Strawberry Cream Cake Bites</a>
+            </figcaption>
+          </figure>
+        </div>
+        <div class="col-lg-3 col-md-4 col-6">
+          <figure class="my-3 my-md-4 tstbite-card">
+            <a href="single-recipe.php" class="tstbite-animation stretched-link rounded-6">
+              <img src="assets/images/menus/menu140.jpg" class="w-100" alt="Menu">
+            </a>
+            <figcaption class="mt-2">
+              <a href="single-recipe.php" class="text-black d-block mt-1 font-weight-semibold big">Tiramisu Cheescake</a>
+            </figcaption>
+          </figure>
+        </div>
+      </div>
+      <div class="text-center py-5">
+        <a href="#0" class="btn btn-outline-dark px-4 px-md-5 py-1 py-md-2 big font-weight-medium">Load More</a>
+      </div>
+    </section>
+  </div>
+  
+<?php include('footer.php') ?>
